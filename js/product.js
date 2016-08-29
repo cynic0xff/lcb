0a5b9434f24fd80b8f8ccbaf3da45611b249ea38
Scott Wren <cynics0xff@gmail.com>
2016-08-29 18:15:02 -0500

    Added price per cup calculation to the add product which is activated when the price field has been filled out and a weight button has been selected

--- js/product.js
+++ js/product.js
@@ -1,25 +1,74 @@
 (function() {
 
-    //initialze jQuery
+    /* wait for jQuery to initialze */
     $(init);
 
+    /* initialze our code */
     function init() {
-        calcPricePerCup();
+        initWeightButtons();
     }
 
-    function calcPricePerCup()
-    {
-        var weight, price, cups, pricePerCup;
-        const SERVING_SIZE = 7;
-        weight = 227;
-        price = 7.99;
+    /* wire up the click events for each button */
+    /* TODO: Look into looping through these */
+    function initWeightButtons() {
+        $("#btn227").click(function(){
+            deactivateButtons();
+            var weight = setActiveButton(0);
+            calcPricePerCup(weight);
+        });
+        
+        $("#btn500").click(function() {
+            deactivateButtons();
+            var weight = setActiveButton(1);
+            calcPricePerCup(weight);
+        });
 
-        //calculat the number of cups based on the weight
+        $("#btn1000").click(function() {
+            deactivateButtons();
+            var weight = setActiveButton(2);
+            calcPricePerCup(weight);
+        });
+
+        $("#btn6000").click(function() {
+            deactivateButtons();
+            var weight = setActiveButton(3);
+            calcPricePerCup(weight);
+        });
+    }
+
+    /* deactive all the buttons */
+    function deactivateButtons() {
+        //remove the class
+        $('.btn-group > .btn').removeClass('active')
+    }
+
+    /* set the button that has been clicked to active class */
+    function setActiveButton(index) {
+        //set the class to active so that it is highlighted
+        $('.btn-group > .btn').eq(index).addClass('active');
+
+        //return the value of the active button
+        return weight = $('.btn-group > .btn.active').val();
+    }
+
+    /* calculate the price per cup */
+    /* formula: cups = weight / SERVING_SIZE */
+    /*        : pricePerCup = price / cups */
+    function calcPricePerCup(weight)
+    {
+        const SERVING_SIZE = 7;
+        var price, cups, pricePerCup;
+        
+        //get the price entered
+        price = $('#price').val();
+
+        //calculate the number of cups based on the weight
         cups = weight / SERVING_SIZE;
         
         //get the price per cup
         pricePerCup = (price / cups);
 
+        //update the gui
         $('#ppc').text(pricePerCup.toFixed(2));
     }
 })();
